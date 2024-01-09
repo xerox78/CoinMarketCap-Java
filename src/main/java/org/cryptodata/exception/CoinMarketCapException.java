d8771b97906bde069e58414a4b7609fc165f2ddc
xerox78 <xerox78@outlook.pt>
2024-01-09 15:34:56 +0000

    Added constructor with throwable

--- src/main/java/org/cryptodata/exception/CoinMarketCapException.java
+++ src/main/java/org/cryptodata/exception/CoinMarketCapException.java
@@ -2,6 +2,9 @@ package org.cryptodata.exception;
 
 public class CoinMarketCapException extends Exception {
 
+    public CoinMarketCapException(String message, Throwable cause) {
+        super(message, cause);
+    }
     public CoinMarketCapException(String message) {
         super(message);
     }
